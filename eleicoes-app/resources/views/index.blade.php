980eb4b0ac8fcd4779f6347ec885f744b253315a
Silva Soluções Tech <silvasolucoes2023@gmail.com>
2024-07-13 10:55:25 -0300

    Atualizar

--- eleicoes-app/resources/views/index.blade.php
+++ eleicoes-app/resources/views/index.blade.php
@@ -8,10 +8,10 @@
     </div>
     <div class="p-2 pb-3 md:p-3 md:pt-4 bg-white rounded shadow-md text-gray-800 mb-3">
         <div class="flex flex-row items-center">
-            <div class="xs:text-lg font-bold"> 100,00% das seções totalizadas <br>
-                <small class="text-roxo text-xs font-bold">(Horário local)</small>
+            <div class="xs:text-lg font-bold"> Transmissão Pendente <br>
+                <small class="text-roxo text-xs font-bold">(Dados acumulados prontos para envio)</small>
             </div>
-            <button class="btn-atualizar ml-auto"><img src="{{ asset('img/atualizar.gif') }}" width="15" alt="Atualizar Icon"> Transferir</button>
+            <button class="btn-atualizar ml-auto"><img src="{{ asset('img/atualizar.gif') }}" width="15" alt="Atualizar Icon" class="me-1"> Transferir</button>
         </div>
         <div class="mt-2 md:mt-3 bg-gray-400 rounded-2xs overflow-hidden">
             <div class="barra-preenchida rounded-2xs" style="width: 100%;"></div>
--- eleicoes-app/resources/views/layout.blade.php
+++ eleicoes-app/resources/views/layout.blade.php
@@ -85,10 +85,6 @@
         <div class="container copyright text-center mt-4">
             <p>© <span>Copyright</span> <strong class="px-1 sitename">Pesquisa Política</strong><span>Todos os direitos reservados</span></p>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you've purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
                 Versão <span>1.0</span>
             </div>
         </div>
